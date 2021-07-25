d0358bf68eb9c2b0613f6bc9254c112a7584c7be
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-25 16:22:55 +0200

    static serve

--- root/urls.py
+++ root/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('app.urls', namespace='main')),
     path('accounts/', include('accounts.urls', namespace='accounts'))
 ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
