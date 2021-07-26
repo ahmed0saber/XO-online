3c760dcaf98a3bbd8d3ba399185e74599c6c9659
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-07-26 14:19:55 +0200

    added fields to user

--- accounts/models.py
+++ accounts/models.py
@@ -1,16 +1,18 @@
 from django.db import models
-
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import Manager
 
+import uuid
 # Create your models here.
 
 class CustomUser(AbstractUser):
     username = None
     email = models.EmailField(_('email address'), unique=True)
-
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
