d5a493c3beb9a4f4139b126db3c861e7597de4ac
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-08-10 22:57:19 +0200

    messages model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from accounts.models import CustomUser
 
 # Create your models here.
+class global_message(models.Model):
+    sender = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+    date_sent = models.DateTimeField(auto_now_add=True)
+    content =  models.TextField()
+
+    def __str__(self):
+        return self.content[:20]
