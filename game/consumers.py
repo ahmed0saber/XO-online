c8a3837d8534101cc0958475e2df71999d8cbf7b
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-08-15 03:00:17 +0200

    game socket connections

--- game/consumers.py
+++ game/consumers.py
@@ -7,27 +7,50 @@ class GameConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.room_name = self.scope['url_route']['kwargs']['room']
         self.room_group_name = 'game_' + self.room_name
-        if len(self.channel_layer.groups.get('self.room_group_name', {}).items()) > 1:
+        connected_count = len(self.channel_layer.groups.get(self.room_group_name, {}).items())
+        if connected_count > 1:
+            await self.accept()
             await self.send(text_data=json.dumps({'error': 'this room is already full try to connect to different room'}))
             await self.close()
             return
         if self.scope['user'].is_authenticated:
-
             await self.channel_layer.group_add(
                 self.room_group_name,
                 self.channel_name
             )
-
             await self.accept()
-            
+            await self.send(json.dumps({'first': connected_count == 0}))
         else:
-            await self.send(text_data=json.dumps({'error': 'you have to login first to be able to play online'}))
             await self.close()
 
     
     async def disconnect(self, close_code):
         # Leave room group
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':'user_left'
+            }
+        )
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
         )
+    
+    async def receive(self, text_data):
+        text_data = json.loads(text_data)
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type':'game_play',
+                'player':text_data['player'],
+                'move':text_data['move']
+            }
+        )
+    
+
+    async def game_play(self, event):
+        await self.send(text_data=json.dumps(event))
+    
+    async def user_left(self, event):
+        await self.send(json.dumps(event))
--- root/routing.py
+++ root/routing.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.urls.conf import include
 from chat.consumers import GlobalChatConsumer
+from game.consumers import GameConsumer
 
 websocket_urlpatterns = [
     path('ws/chat/', GlobalChatConsumer.as_asgi()),
-    # path('ws/game/', )
+    path('ws/game/<str:room>/', GameConsumer.as_asgi())
 ]
