24f6d79e127b10863edbed8e4e1c15d4a708889e
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-08-23 22:20:55 +0200

    remove password validator suer attribute similar

--- root/settings.py
+++ root/settings.py
@@ -145,10 +145,9 @@ LOGGING = {
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
+
 AUTH_PASSWORD_VALIDATORS = [
-    {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
+
     {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
     },
