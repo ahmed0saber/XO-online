b6ee0e0585e26d467e6afa63ffddd9200ce42cd9
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-09-20 17:17:19 +0200

    icon and manifest

--- root/urls.py
+++ root/urls.py
@@ -28,6 +28,7 @@ urlpatterns = [
     path('chat/', include('chat.urls', namespace='chat')),
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
