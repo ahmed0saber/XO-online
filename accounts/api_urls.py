d781e62773b5e3ca503298fa23792be352497586
Ahmed Yasser <ahmedahmedyasseryasser@gmail.com>
2021-09-20 16:08:34 +0200

    validate fields on signup

--- accounts/api_urls.py
+++ accounts/api_urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 
 app_name = 'api'
 urlpatterns = [
-    path('check_email/', check_email, name='check_email')
+    path('check_email/', check_email, name='check_email'),
+    path('check_password/', check_password, name='check_password'),
 ]
--- accounts/api_views.py
+++ accounts/api_views.py
@@ -2,19 +2,33 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
 from django.core.validators import validate_email
+from django.contrib.auth.password_validation import validate_password
+
 from django.core.exceptions import ValidationError
 from .models import CustomUser
 
 
 @api_view(['GET'])
 def check_email(request):
-    mail = request.GET.get('email', None)
+    mail = request.query_params.get('email', None)
     try:
         validate_email(mail)
         query = CustomUser.objects.filter(email=mail)        
         if query.exists():
-            return Response({"error":"This email already has account"}, status=status.HTTP_409_CONFLICT)
+            return Response({"error":"already exists", "details":["This email already has an account"]}, status=status.HTTP_409_CONFLICT)
         else:
             return Response({"success":"This email is valid"}, status=status.HTTP_200_OK)
     except ValidationError as e:
-        return Response({"error":"This email is invalid please enter a valid email address"}, status=status.HTTP_406_NOT_ACCEPTABLE)
+        return Response({"error":"This email is invalid please enter a valid email address","details":e}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+@api_view(['GET'])
+def check_password(request):
+    password = request.query_params.get('password')
+    try:
+        validate_password(password)
+        return Response({"success":"This password is valid"}, status=status.HTTP_200_OK)
+    except ValidationError as e:
+        return Response({"error":"This password is invalid please enter a valid password","details":e}, status=status.HTTP_406_NOT_ACCEPTABLE)
+
+
+    
